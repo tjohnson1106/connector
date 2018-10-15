0185dcde6e9bf44c152b9efb383bf2461e075339
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-15 15:50:08 -0400

    created register post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
 
 const User = require("../../models/User");
 
@@ -12,6 +13,25 @@ router.get("/test", (req, res) =>
 router.post("/register", (req, res) => {
   User.findOne({
     email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "email already exists"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "r", //rating
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
   });
 });
 
