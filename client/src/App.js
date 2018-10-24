e499ae25825d8f059cae6f5065c45635a1238383
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-24 16:36:05 -0400

    created basic footer and navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navbar from "./components/layout/NavBar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Connector</h1>
+        <Footer />
       </div>
     );
   }
