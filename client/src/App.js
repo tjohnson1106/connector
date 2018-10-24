d894ee54eba2ede1fc68f9b353450b0c04480200
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-24 16:41:56 -0400

    created basic landing page layout

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <h1>Connector</h1>
+        <Landing />
         <Footer />
       </div>
     );
