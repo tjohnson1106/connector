256c68eac91a925bee3785875bc66c3277475729
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-26 13:15:22 -0400

    created current user action/reducer and implemented client side empty validation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
 // Register User
@@ -33,6 +34,14 @@ export const loginUser = userData => dispatch => {
       // Set token to auth header
 
       setAuthToken(token);
+
+      // Decode token to obtain user data
+
+      const decoded = jwt_decode(token);
+
+      // Set current user
+
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err =>
       dispatch({
@@ -40,4 +49,13 @@ export const loginUser = userData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
