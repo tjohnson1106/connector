28870e8e349b88bfa21105e2cff69e39ca299fd7
Thomas Johnson <tjohnson1106@gmail.com>
2018-10-29 14:58:21 -0400

    implemented create profile scaffolding

---
+++ client/src/components/create-profile/CreateProfile/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+// TODO: Create profille UI 102920181457
+
+class CreateProfile extends Component {
+  state = {};
+  render() {
+    return <div>{}</div>;
+  }
+}
+
+export default CreateProfile;
