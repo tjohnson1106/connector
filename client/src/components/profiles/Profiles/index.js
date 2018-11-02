d80ed1c24bfc50ada409c2191679842cabddfce3
Thomas Johnson <tjohnson1106@gmail.com>
2018-11-02 14:11:18 -0400

    created profile items component

--- client/src/components/profiles/Profiles/index.js
+++ client/src/components/profiles/Profiles/index.js
@@ -11,7 +11,7 @@ class Profiles extends Component {
   }
 
   render() {
-    const { profiles, loading } = this.state.profile;
+    const { profiles, loading } = this.props.profile;
     let profileItems;
 
     if (profiles === null || loading) {
